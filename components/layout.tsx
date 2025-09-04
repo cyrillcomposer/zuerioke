2e42b4bed47f3c089b685851f69125ffcee58967
cyrillcomposer <pianotuition@gmail.com>
2025-09-04 20:51:27 +0200

    switch components to lowercase + fix imports

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
-import Navigation from "./Navigation";
-import Footer from "./Footer";
+import Navigation from "./navigation";
+import Footer from "./footer";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
